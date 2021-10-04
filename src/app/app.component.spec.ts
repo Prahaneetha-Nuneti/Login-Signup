d2cb426e93ee907da943556e51baa3ff9918652b
Prahaneetha-Nuneti <b18cs081@kitsw.ac.in>
2021-10-04 18:57:06 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularDemo'`, () => {
+  it(`should have as title 'tasklist'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularDemo');
+    expect(app.title).toEqual('tasklist');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angularDemo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('tasklist app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,48 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularDemo';
-  counter = 0;
-  
-
-  cards=[
-    {
-      cardtitle : "Free",
-      cardcost : 0,
-      noofusers : "Single user",
-      storage : "5GB Storage",
-      project : "Unlimited Public Projects",
-      access : "Community Access",
-      privateproject : false,
-      phonesupport : false,
-      subdomain : false,
-    status : false
-    },
-    {
-      cardtitle : "Plus",
-      cardcost: 9,
-      noofusers : "5 Users",
-      storage : "50GB Storage",
-      project : "Unlimited Public Projects",
-      access : "Community Access",
-      privateproject : true,
-      phonesupport : true,
-      subdomain : true,
-    status: false
-    },
-    {
-      cardtitle : "Pro",
-      cardcost : 49,
-     noofusers : "Unlimited Users",
-      storage : "150GB Storage",
-      project : "Unlimited Public Projects",
-      access : "Community Access",
-      privateproject : true,
-      phonesupport : true,
-      subdomain : true,
-     status: true
-    }
-  ]
-
-  
+  title = 'tasklist';
 }
